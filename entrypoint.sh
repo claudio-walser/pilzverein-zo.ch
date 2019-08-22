02a9dad1666e82ce515fc1756e3f3d90f8a8773a
Claudio Walser <claudio.walser@srf.ch>
2019-08-22 17:12:50 +0200

    Ensure /var/www/html/neos/ is a directory, even if not mounted from host

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /var/www/html/neos/
+
 if [ `ls -A /var/www/html/neos/ | wc -m` == "0" ]; then
     echo "Moving initial application code."
     mv /var/www/html/neos-orig/* /var/www/html/neos/;
